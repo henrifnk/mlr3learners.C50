36028dd5fab56067f66f62b54841061e1f024723
HenriLMU <50552831+HenriLMU@users.noreply.github.com>
2020-03-21 18:15:35 +0100

    include c5Control fun

--- R/LearnerClassifC50.R
+++ R/LearnerClassifC50.R
@@ -22,18 +22,21 @@ LearnerClassifC5.0 = R6Class("LearnerClassifC50",
         params = list(
           ParamInt$new(id = "trials", default = 1L, lower = 1L, tags = c("train", "predict")),
           ParamLgl$new(id = "rules", default = FALSE, tags = "train"),
-          ParamLgl$new(id = "subset", default = FALSE, tags = "train"),
-          ParamInt$new(id = "bands", lower = 2L, upper = 1000L, tags = "train"),
-          ParamLgl$new(id = "winnow", default = FALSE, tags = "train"),
-          ParamLgl$new(id = "noGlobalPruning", default = FALSE, tags = "train"),
-          ParamDbl$new(id = "CF", lower = 0, upper = 1, default = 0.25, tags = "train"),
-          ParamInt$new(id = "minCases", lower = 0L, upper = Inf, default = 2L, tags = "train"),
-          ParamLgl$new(id = "fuzzyThreshold", default = FALSE, tags = "train"),
-          ParamDbl$new(id = "sample", lower = 0, upper = .999, default = 0, tags = "train"),
-          ParamInt$new(id = "seed", lower = -Inf, upper = Inf, tags = "train"),
-          ParamLgl$new(id = "earlyStopping", default = TRUE, tags = "train")
+          ParamLgl$new(id = "subset", default = FALSE, tags = c("train", "C5.0Control")),
+          ParamInt$new(id = "bands", lower = 2L, upper = 1000L, tags = c("train", "C5.0Control")),
+          ParamLgl$new(id = "winnow", default = FALSE, tags = c("train", "C5.0Control")),
+          ParamLgl$new(id = "noGlobalPruning", default = FALSE, tags = c("train", "C5.0Control")),
+          ParamDbl$new(id = "CF", lower = 0, upper = 1, default = 0.25, tags = c("train", "C5.0Control")),
+          ParamInt$new(id = "minCases", lower = 0L, upper = Inf, default = 2L, tags = c("train", "C5.0Control")),
+          ParamLgl$new(id = "fuzzyThreshold", default = FALSE, tags = c("train", "C5.0Control")),
+          ParamDbl$new(id = "sample", lower = 0, upper = .999, default = 0, tags = c("train", "C5.0Control")),
+          ParamInt$new(id = "seed", lower = -Inf, upper = Inf, tags = c("train", "C5.0Control")),
+          ParamLgl$new(id = "earlyStopping", default = TRUE, tags = c("train", "C5.0Control")),
+          ParamUty$new(id = "label", default = "outcome", tags = c("train", "C5.0Control"))
         )
       )
+      # bands depends on rules being TRUE
+      ps$add_dep("bands", "rules", CondEqual$new(TRUE))
       
       super$initialize(
         # see the mlr3book for a description: https://mlr3book.mlr-org.com/extending-mlr3.html
@@ -42,8 +45,8 @@ LearnerClassifC5.0 = R6Class("LearnerClassifC50",
         feature_types = c("numeric", "factor", "ordered"),
         predict_types = c("response", "prob"),
         param_set = ps,
-        properties =c("twoclass", "multiclass", "missings", "weights")
-     
+        properties = c("twoclass", "multiclass", "missings", "weights"),
+        man = "mlr3learners.C50::mlr_learners_classif.C50"
         )
       }
     ),
@@ -51,23 +54,25 @@ LearnerClassifC5.0 = R6Class("LearnerClassifC50",
 
   private = list(
     .train = function(task) {
+      # Extrakt Control Parameters from Param List to call them separatly
+      c5control = do.call(C50::C5.0Control,
+                          self$param_set$get_values(tags = "C5.0Control"))
+      
       pars = self$param_set$get_values(tags = "train")
-
       # Get formula, data, classwt, cutoff for the randomForest
       f = task$formula() #the formula is available in the task
       data = task$data() #the data is avail
       # use the mlr3misc::invoke function (it's similar to do.call())
-      invoke(C50::C5.0.formula, formula = f, data = data, .args = pars)
+      invoke(C50::C5.0.formula, formula = f, data = data, control = c5control, .args = pars)
     },
 
     .predict = function(task) {
-      self$predict_type = "response"
       response = NULL
       prob = NULL
       pars = self$param_set$get_values(tags = "predict")
       newdata = task$data(cols = task$feature_names) # get newdata
       
-      if(self$predict_type == "response") {
+      if (self$predict_type == "response") {
         response = invoke(predict, self$model, newdata = newdata,
                           type = "class", .args = pars)
       } else {
